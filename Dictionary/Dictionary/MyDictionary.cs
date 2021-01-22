17f4479334f5e7b76f2b3d087f40798222305cb0
Anıl ARSLAN <anlarsln0@gmail.com>
2021-01-23 01:13:31 +0300

    Dictionary
    Dictionary Project

---
+++ Dictionary/Dictionary/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<Key, Value>
+    {
+
+        Key[] keys;
+        Value[] values;
+
+
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+
+
+        public void Add(Key key, Value value)
+        {
+            Key[] _tempKeys = keys;
+            Value[] _tempValues = values;
+
+
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < _tempKeys.Length; i++)
+            {
+                keys[i] = _tempKeys[i];
+                values[i] = _tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+
+        public Key[] Keys
+        {
+            get {return keys;}
+        }
+
+        public Value[] Values
+        {
+            get { return values; }
+        }
+
+
+
+    }
+}
---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cities = new MyDictionary<int, string>();
+            cities.Add(01, "Adana");
+            cities.Add(61, "Trabzon");
+            cities.Add(34, "İstanbul");
+            cities.Add(54, "Sakarya");
+            cities.Add(82, "Paris");
+
+            for (int i = 0; i < cities.Keys.Length; i++)
+            {
+                Console.WriteLine("Key: {0}, Value: {1} ", cities.Keys[i], cities.Values[i]);
+            }
+        }
+    }
+}
